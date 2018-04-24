c647d1e7084b21c1c45dd71d9517173307400367
thinkmix <thinkmix@163.com>
2018-04-24 14:43:38 +0800

    修改静态资源文件；ym

--- src/views/notice/notice.vue
+++ src/views/notice/notice.vue
@@ -99,10 +99,10 @@
                         <td>
                             <div class="msgTime"><span class="timeBig">{{getDateTime(v.creatTime)[2]}}日</span> {{getDateTime(v.creatTime)[1]}}月 {{getDateTime(v.creatTime)[0]}}年&nbsp;&nbsp;{{getDateTime(v.creatTime)[5]}}<span class="fr">有效期 ：{{getDateTime(v.expTime)[0]}}年{{getDateTime(v.expTime)[1]}}月{{getDateTime(v.expTime)[2]}}日</span></div>
                             <div class="msgInfo">
-                                <p><img class="icon" src="/static/icon/book.svg" alt="11" /><span class="c-green"><span v-if="v.type=='600'">(系统消息)</span><span v-if="v.type=='200'">(政策消息)</span><span v-if="v.type=='500'">(优惠消息)</span><span v-if="v.type=='400'">(新货上架)</span></span><span v-if="v.content!=''">{{v.content}}</span></p>
-                                <p><img class="icon" src="/static/icon/link1.svg" alt="22" /><a v-if="v.redirectUrl!=''" :href=v.redirectUrl target="_blank" class="c-blue textDec">{{v.redirectUrl}}</a></p>
+                                <p><img class="icon" src="../../assets/images/icon/book.svg" alt="11" /><span class="c-green"><span v-if="v.type=='600'">(系统消息)</span><span v-if="v.type=='200'">(政策消息)</span><span v-if="v.type=='500'">(优惠消息)</span><span v-if="v.type=='400'">(新货上架)</span></span><span v-if="v.content!=''">{{v.content}}</span></p>
+                                <p><img class="icon" src="../../assets/images/icon/link1.svg" alt="22" /><a v-if="v.redirectUrl!=''" :href=v.redirectUrl target="_blank" class="c-blue textDec">{{v.redirectUrl}}</a></p>
                                 <p >
-                                    <img class="icon" src="/static/icon/link.svg" alt="33" />
+                                    <img class="icon" src="../../assets/images/icon/link.svg" alt="33" />
                                     附件&nbsp;:&nbsp;<a v-if="v.annex.length>0" @click="imgBigFunction(i)" href="javascript:void(0)" class="c-yellow textDec">{{v.annex[0].fileName}}</a><img v-if="v.annex.length>0" ref="imgBigFunction" :src=v.annex[0].base64String @click="clickImg($event)" class="imgS">
                                     <span class="fr">
                                         发送对象:<a v-if="v.receiverType==1" href="javascript:void(0)" @click="checkSendUser(1,v)">全部</a>
@@ -190,6 +190,8 @@ import {requestMethod} from "../../config/service.js";
 import { errorDeal,getDateTime } from '../../config/utils';
 import layerConfirm from "../../../components/layerConfirm";
 import imgBiger from "../../../components/ImgBiger";
+import successIcon from "../../assets/images/icon/success-circle.svg"
+import errorIcon from "../../assets/images/icon/ava_error.svg"
 export default {
     data(){
         return{
@@ -353,7 +355,7 @@ export default {
               let data={"image":base64},url="/yfd-mns/w/msg/imgUpload";
               requestMethod(data,url)
               .then((resdata)=>{
-                    vm.valuesrc="/static/icon/success-circle.svg";
+                    vm.valuesrc=successIcon;
                     vm.off.imgIcon=true;
                     vm.upLoad=false;
                     vm.imgFile=resdata.data;
@@ -366,7 +368,7 @@ export default {
                         msgSkin:'success',
                     })
                   }else{
-                    vm.valuesrc="/static/icon/ava_error.svg";
+                    vm.valuesrc=errorIcon;
                     vm.off.imgIcon=true;                      
                     vm.upLoad=false;
                     layer.open({
